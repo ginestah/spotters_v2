146496b9d440a6d41d529b32b684dc9b6748058f
ginestah <68362184+ginestah@users.noreply.github.com>
2021-05-12 09:15:57 -0600

    first commit to github
    starting a more robust spotters site

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :password, length: {minimum: 6}
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
